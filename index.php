620a9a6302cb2560237a87bf6294e0022980f17e
Hussein H. Ahmed <44538768+husseiinhamdy97@users.noreply.github.com>
2025-12-19 19:56:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+header('Content-Type: application/json');
 
-echo "Hello from Fly PHP 22";
+$response = [
+  "server_ip" => $_SERVER['SERVER_ADDR'] ?? null,
+  "client_ip" => $_SERVER['HTTP_X_FORWARDED_FOR']
+    ?? $_SERVER['HTTP_CF_CONNECTING_IP']
+    ?? $_SERVER['REMOTE_ADDR']
+    ?? null,
+  "headers" => getallheaders()
+];
 
+echo json_encode($response, JSON_PRETTY_PRINT);
