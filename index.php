4332227795ed91a2c31d02d64e1970c9edcd7b15
Hussein H. Ahmed <44538768+husseiinhamdy97@users.noreply.github.com>
2025-12-19 20:21:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-header('Content-Type: application/json');
-
-$response = [
-  "server_ip" => $_SERVER['SERVER_ADDR'] ?? null,
-  "client_ip" => $_SERVER['HTTP_X_FORWARDED_FOR']
-    ?? $_SERVER['HTTP_CF_CONNECTING_IP']
-    ?? $_SERVER['REMOTE_ADDR']
-    ?? null,
-  "headers" => getallheaders()
-];
-
-echo json_encode($response, JSON_PRETTY_PRINT);
+echo $_SERVER['HTTP_FLY_CLIENT_IP']
+  ?? explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '')[0]
+  ?? $_SERVER['REMOTE_ADDR'];
